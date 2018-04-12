ea54c3803d735179d58026e203570f074a76fcf7
Owain Kenway <o.kenway@ucl.ac.uk>
2018-04-12 15:56:55 +0100

    `s

--- update_links.sh
+++ update_links.sh
@@ -6,10 +6,11 @@ base_dir=/shared/ucl/apps/restricted_modules
 
 for a in `cat modules.list`
 do
-  mpath=dirname $a
-  mname=basename $a
+  echo "Linking $a"
+  mpath=`dirname $a`
+  mname=`basename $a`
   mkdir -p $base_dir/modulefiles/$mpath
-  ln -s $base_dir/uclmodules/$a $base_dir/modulefiles/$a
+  ln -f -s $base_dir/uclmodules/$a $base_dir/modulefiles/$a
 
 done
 
